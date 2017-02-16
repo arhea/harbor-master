7758da123da896b80162362ff399507a26ba3c9a
Alex Rhea <alex.rhea@gmail.com>
2017-02-16 12:10:31 -0500

    updates for v1.26

--- examples/info.js
+++ examples/info.js
@@ -11,3 +11,9 @@ client.info().then((info) => {
 }).catch((err) => {
   console.error(err);
 });
+
+client.swarm().inspect().then((info) => {
+  console.log(info);
+}).catch((err) => {
+  console.error(err);
+});
